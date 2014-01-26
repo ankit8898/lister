cca1ae34ec539a77fad669abf35427f885346027
Ankit Gupta <ankit.gupta8898@gmail.com>
2014-01-26 11:22:32 -0500

    Moved out into separate

--- yeoman-angular/app/scripts/filters/userFilter.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('angularDemoApp')
-  .filter('reverse', function () {
-    return function (arr) {
-      return {message: "From Method", bb: arr.reverse()};
-    };
-  });
